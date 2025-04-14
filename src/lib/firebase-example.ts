19a5c3cd262dfa164566dd5703981aa71d3f1c76
David Asaolu <asaoludavid234@yahoo.com>
2025-04-14 17:58:37 +0100

    Create firebase-example.ts

---
+++ src/lib/firebase-example.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { EmailAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  // -- firebase config
+};
+
+// Initialize Firebase
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const provider = new EmailAuthProvider();
+const storage = getStorage(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { provider, auth, storage };
+export default db;
